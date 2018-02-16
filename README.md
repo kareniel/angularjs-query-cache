a220b82a71ad1694dba542392729ea29489e7a55
kareniel <jonathan.dupre@gmail.com>
2018-02-16 10:10:32 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # angularjs-query-cache
-cache response to queries made using the $http service
+
+<div align="left">
+  <!-- Stability -->
+  <a href="https://nodejs.org/api/documentation.html#documentation_stability_index">
+    <img src="https://img.shields.io/badge/stability-unstable-green.svg?style=flat-square"
+      alt="API stability" />
+  </a>
+  <!-- NPM version -->
+  <a href="https://npmjs.org/package/angularjs-query-cache">
+    <img src="https://img.shields.io/npm/v/angularjs-query-cache.svg?style=flat-square"
+      alt="NPM version" />
+  </a>
+  <!-- Downloads -->
+  <a href="https://npmjs.org/package/angularjs-query-cache">
+    <img src="https://img.shields.io/npm/dm/angularjs-query-cache.svg?style=flat-square"
+      alt="Downloads" />
+  </a>
+  <!-- Standard -->
+  <a href="https://standardjs.com">
+    <img src="https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square"
+      alt="Standard" />
+  </a>
+</div>
+
+<br />
+
+Sometimes you want to keep the result to a query you made previously.
+This module decorates the $http service to add better caching.
+
+
+## Usage
+
+Set the `_cache` field to `true` in the `options` object when making a get request using $http.
+Optionally, you can see the max age (in milliseconds) on individual calls through the `_maxAge` key. 
+
+
+```js
+var opts = { 
+  _cache: true, 
+  _maxAge: 5 * 60 * 1000 
+}
+
+$http.get('https://api.my-service.io/endpoint', opts)
+```
+
+## Installation
+
+Using npm:
+
+``npm install --save angularjs-query-cache`
+
---
+++ index.js
@@ -0,0 +1,103 @@
+queryCache.$inject = ['$provide']
+
+const STORAGE_KEY = 'angularjs_query_cache'
+const MINUTE = 60 * 1000
+const DEFAULT_MAX_AGE = 5 * MINUTE
+
+module.exports = angular.module('angularjs-query-cache', [])
+  .config(queryCache)
+  .name
+
+function queryCache ($provide) {
+  $provide.decorator('$http', $httpDecorator)
+}
+
+function $httpDecorator ($delegate, $q) {
+  var old = angular.copy($delegate.get)
+  var cache = new Cache()
+
+  $delegate.get = decoratedGet
+
+  return $delegate
+
+  function decoratedGet (url, config) {
+    var isTemplate = typeof url === 'string' && url.indexOf('.html') !== -1
+
+    if (isTemplate) return old(url, config)
+
+    if (cache.contains(url)) return Promise.resolve(cache.get(url))
+
+    return old(url, config).then(res => {
+      if (config && config._cache && !isTemplate) {
+        cache.set(url, res, config._maxAge)
+      }
+
+      return res
+    })
+  }
+}
+
+function Cache () {
+  this._cache = this._load()
+  this._timer = setInterval(this._clean.bind(this), MINUTE)
+
+  this._clean()
+}
+
+Cache.prototype.get = function (url) {
+  return angular.copy(this._cache[url])
+}
+
+Cache.prototype.set = function (url, data, maxAge) {
+  this._cache[url] = Object.assign(angular.copy(data), {
+    _cachedOn: new Date(),
+    _maxAge: maxAge || DEFAULT_MAX_AGE
+  })
+
+  this._save()
+}
+
+Cache.prototype.contains = function (url) {
+  return !!this._cache[url]
+}
+
+Cache.prototype._remove = function (url) {
+  delete this._cache[url]
+}
+
+Cache.prototype._clean = function () {
+  var now = new Date()
+  var delta
+
+  for (var key in this._cache) {
+    delta = now - this._cache[key]._cachedOn
+
+    if (delta > this._cache[key]._maxAge) {
+      this._remove(key)
+    }
+  }
+
+  this._save()
+}
+
+Cache.prototype._save = function () {
+  window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(this._cache))
+}
+
+Cache.prototype._load = function () {
+  var cache = window.sessionStorage.getItem(STORAGE_KEY)
+
+  if (!cache) return {}
+
+  try {
+    cache = JSON.parse(cache)
+
+    for (var key in cache) {
+      cache[key]._cachedOn = new Date(cache[key]._cachedOn)
+    }
+
+    return cache
+  } catch (err) {
+    return {}
+  }
+}
